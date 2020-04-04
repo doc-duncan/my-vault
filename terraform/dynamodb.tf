33fa25666f70e71bdfaa5076b2624da561b713c3
Doc Duncan <triplerockmentality@icloud.com>
2020-04-04 17:08:37 -0400

    DMD (D) login table tf

---
+++ terraform/dynamodb.tf
@@ -0,0 +1,14 @@
+resource "aws_dynamodb_table" "login" {
+  name           = "login"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "app"
+
+  attribute {
+    name = "app"
+    type = "S"
+  }
+
+  tags = {
+    project = "my-vault"
+  }
+}
