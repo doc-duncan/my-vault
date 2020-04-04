b5b83325a2c392213b53b2499a1f3120656b4717
Doc Duncan <triplerockmentality@icloud.com>
2020-04-04 16:05:35 -0400

    DMD (D) my-vault-key-pair tf

---
+++ terraform/kms.tf
@@ -0,0 +1,15 @@
+resource "aws_kms_key" "my-vault-key-pair" {
+  description              = "KMS key pair for my-vault"
+  deletion_window_in_days  = 7
+  key_usage                = "ENCRYPT_DECRYPT"
+  customer_master_key_spec = "RSA_2048"
+  tags = {
+    project = "my-vault"
+  }
+}
+
+resource "aws_kms_alias" "my-vault-key-pair" {
+  name          = "alias/my-vault-key-pair"
+  target_key_id = aws_kms_key.my-vault-key-pair.arn 
+}
+
