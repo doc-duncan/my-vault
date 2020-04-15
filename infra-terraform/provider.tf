71e74fcaada2a995e289f962882c9006cfb3bb8a
Doc Duncan <triplerockmentality@icloud.com>
2020-04-15 15:40:56 -0400

    DMD (D) added remote backend for state

--- infra-terraform/provider.tf
+++ infra-terraform/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">=0.12.20"
+  backend "s3" {
+    bucket         = "my-vault-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region  = "us-east-1"
   profile = "default"
